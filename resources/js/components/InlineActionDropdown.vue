8cd1ebc68ecce9634e2f167305b2f207ae42a782
Hubert Smusz <hubert.smusz@movecloser.pl>
2024-04-18 10:17:27 +0200

    updated from https://codeberg.org/Lednerb/nova-action-button-selectors

--- resources/js/components/InlineActionDropdown.vue
+++ resources/js/components/InlineActionDropdown.vue
@@ -1,209 +1,199 @@
 <template>
-    <div v-if="shouldShowDropdown" class="flex">
-         <div
-            v-if="actions.length > 0"
-            :dusk="`${resource.id.value}-inline-actions`"
-            class="flex gap-4 py-1"
-        >
+    <div class="flex">
+        <div v-if="actions.length > 0" :dusk="`${resource.id.value}-inline-actions`" class="mr-4 flex gap-4">
             <!-- User Actions -->
             <template v-for="action in actions">
-              <button
-              as="button"
-              v-if="!action.showInDropdown"
-              :key="action.uriKey"
-              :dusk="`${resource.id.value}-inline-action-${action.uriKey}`"
-              @click.stop="() => handleActionClick(action.uriKey)"
-              :title="action.name"
-              :destructive="action.destructive"
-              class="items-center justify-center px-3 text-sm font-bold text-white rounded shadow cursor-pointer bg-primary-500 hover:bg-primary-400 dark:text-gray-900 focus:outline-none focus:ring ring-primary-200 dark:ring-gray-600 h-9"
-              >
-              {{ action.name }}
-              </button>
-            </template>
-        </div>
-        
-        <Dropdown>
-        <span class="sr-only">{{ __('Resource Row Dropdown') }}</span>
-        <DropdownTrigger
-            :dusk="`${resource.id.value}-control-selector`"
-            :show-arrow="false"
-        >
-            <span class="py-0.5 px-2 rounded">
-            <Icon :solid="true" type="dots-horizontal" />
-            </span>
-        </DropdownTrigger>
-
-        <template #menu>
-            <DropdownMenu width="auto" class="px-1">
-            <ScrollWrap
-                :height="250"
-                class="divide-y divide-gray-100 dark:divide-gray-800 divide-solid"
-            >
-                <div class="py-1" v-if="userHasAnyOptions">
-                <!-- Preview Resource Link -->
-                <DropdownMenuItem
-                    v-if="shouldShowPreviewLink"
-                    :data-testid="`${resource.id.value}-preview-button`"
-                    :dusk="`${resource.id.value}-preview-button`"
-                    as="button"
-                    @click.prevent="$emit('show-preview')"
-                    :title="__('Preview')"
-                >
-                    {{ __('Preview') }}
-                </DropdownMenuItem>
-
-                <!-- Replicate Resource Link -->
-                <DropdownMenuItem
-                    v-if="resource.authorizedToReplicate"
-                    :dusk="`${resource.id.value}-replicate-button`"
-                    :href="
-                    $url(
-                        `/resources/${resourceName}/${resource.id.value}/replicate`,
-                        { viaResource, viaResourceId, viaRelationship }
-                    )
-                    "
-                    :title="__('Replicate')"
-                >
-                    {{ __('Replicate') }}
-                </DropdownMenuItem>
-
-                <!-- Impersonate Resource Button -->
-                <DropdownMenuItem
-                    as="button"
-                    v-if="canBeImpersonated"
-                    :dusk="`${resource.id.value}-impersonate-button`"
-                    @click.prevent="
-                    startImpersonating({
-                        resource: resourceName,
-                        resourceId: resource.id.value,
-                    })
-                    "
-                    :title="__('Impersonate')"
-                >
-                    {{ __('Impersonate') }}
-                </DropdownMenuItem>
-                </div>
-
-                <div
-                  v-if="actions.length > 0"
-                  :dusk="`${resource.id.value}-inline-actions`"
-                  class="py-1"
-                >
-                  <!-- User Actions -->
-                  <template v-for="action in actions">
-                  <DropdownMenuItem
-                    as="button"
-                    v-if="action.showInDropdown"
+                <button
+                    v-if="action.showAsButton"
                     :key="action.uriKey"
+                    as="button"
                     :dusk="`${resource.id.value}-inline-action-${action.uriKey}`"
-                    @click="() => handleActionClick(action.uriKey)"
                     :title="action.name"
                     :destructive="action.destructive"
-                  >
+                    class="bg-primary-500 hover:bg-primary-400 ring-primary-200 h-9 cursor-pointer items-center justify-center rounded px-3 text-sm font-bold text-white shadow focus:outline-none focus:ring dark:text-gray-900 dark:ring-gray-600"
+                    @click.stop="() => handleActionClick(action.uriKey)"
+                >
                     {{ action.name }}
-                  </DropdownMenuItem>
-                  </template>
+                </button>
+            </template>
+        </div>
+
+        <Dropdown v-if="shouldShowDropdown">
+            <template #default>
+                <div>
+                    <span class="sr-only">{{ __('Resource Row Dropdown') }}</span>
+                    <Button variant="action" icon="ellipsis-horizontal" :dusk="`${resource.id.value}-control-selector`" />
                 </div>
-            </ScrollWrap>
-            </DropdownMenu>
-        </template>
+            </template>
+
+            <template #menu>
+                <DropdownMenu v-if="shouldShowDropdown" width="auto" class="px-1">
+                    <ScrollWrap :height="250" class="divide-y divide-solid divide-gray-100 dark:divide-gray-800">
+                        <div v-if="userHasAnyOptions" class="py-1">
+                            <!-- Preview Resource Link -->
+                            <DropdownMenuItem
+                                v-if="shouldShowPreviewLink"
+                                :data-testid="`${resource.id.value}-preview-button`"
+                                :dusk="`${resource.id.value}-preview-button`"
+                                as="button"
+                                :title="__('Preview')"
+                                @click.prevent="$emit('show-preview')"
+                            >
+                                {{ __('Preview') }}
+                            </DropdownMenuItem>
+
+                            <!-- Replicate Resource Link -->
+                            <DropdownMenuItem
+                                v-if="resource.authorizedToReplicate"
+                                :dusk="`${resource.id.value}-replicate-button`"
+                                :href="
+                                    $url(`/resources/${resourceName}/${resource.id.value}/replicate`, {
+                                        viaResource,
+                                        viaResourceId,
+                                        viaRelationship,
+                                    })
+                                "
+                                :title="__('Replicate')"
+                            >
+                                {{ __('Replicate') }}
+                            </DropdownMenuItem>
+
+                            <!-- Impersonate Resource Button -->
+                            <DropdownMenuItem
+                                v-if="canBeImpersonated"
+                                as="button"
+                                :dusk="`${resource.id.value}-impersonate-button`"
+                                :title="__('Impersonate')"
+                                @click.prevent="
+                                    startImpersonating({
+                                        resource: resourceName,
+                                        resourceId: resource.id.value,
+                                    })
+                                "
+                            >
+                                {{ __('Impersonate') }}
+                            </DropdownMenuItem>
+                        </div>
+
+                        <div v-if="amountDropdownActions > 0" :dusk="`${resource.id.value}-inline-actions`" class="py-1">
+                            <!-- User Actions -->
+                            <template v-for="action in actions">
+                                <DropdownMenuItem
+                                    v-if="!action.showAsButton"
+                                    :key="action.uriKey"
+                                    as="button"
+                                    :dusk="`${resource.id.value}-inline-action-${action.uriKey}`"
+                                    :title="action.name"
+                                    :destructive="action.destructive"
+                                    @click="() => handleActionClick(action.uriKey)"
+                                >
+                                    {{ action.name }}
+                                </DropdownMenuItem>
+                            </template>
+                        </div>
+                    </ScrollWrap>
+                </DropdownMenu>
+            </template>
         </Dropdown>
 
         <!-- Action Confirmation Modal -->
         <component
-        v-if="confirmActionModalOpened"
-        :show="confirmActionModalOpened"
-        :is="selectedAction.component"
-        :working="working"
-        :selected-resources="selectedResources"
-        :resource-name="resourceName"
-        :action="selectedAction"
-        :endpoint="endpoint"
-        :errors="errors"
-        @confirm="executeAction"
-        @close="closeConfirmationModal"
+            :is="selectedAction.component"
+            v-if="confirmActionModalOpened"
+            :show="confirmActionModalOpened"
+            :working="working"
+            :selected-resources="selectedResources"
+            :resource-name="resourceName"
+            :action="selectedAction"
+            :endpoint="endpoint"
+            :errors="errors"
+            @confirm="executeAction"
+            @close="closeConfirmationModal"
         />
 
         <!-- Action Response Modal -->
         <component
-        v-if="selectedAction"
-        :is="actionResponseData.modal"
-        @close="closeActionResponseModal"
-        :show="showActionResponseModal"
-        :data="actionResponseData"
+            :is="actionResponseData.modal"
+            v-if="selectedAction"
+            :show="showActionResponseModal"
+            :data="actionResponseData"
+            @close="closeActionResponseModal"
         />
     </div>
 </template>
 
 <script>
-import { HandlesActions, mapProps } from '@/mixins'
-import { mapGetters, mapActions } from 'vuex'
+import { HandlesActions, mapProps } from '@/mixins';
+import { mapActions, mapGetters } from 'vuex';
+import { Button } from 'laravel-nova-ui';
 
 export default {
-  mixins: [HandlesActions],
+    components: {
+        Button,
+    },
+    mixins: [HandlesActions],
 
-  emits: ['show-preview'],
+    props: {
+        resource: { type: Object },
+        actions: { type: Array },
+        viaManyToMany: { type: Boolean },
 
-  props: {
-    resource: { type: Object },
-    actions: { type: Array },
-    viaManyToMany: { type: Boolean },
-
-    ...mapProps([
-      'resourceName',
-      'viaResource',
-      'viaResourceId',
-      'viaRelationship',
-    ]),
-  },
-
-  data: () => ({
-    showActionResponseModal: false,
-    actionResponseData: {},
-  }),
-
-  methods: mapActions(['startImpersonating']),
-
-  computed: {
-    ...mapGetters(['currentUser']),
-
-    currentSearch() {
-      return ''
+        ...mapProps(['resourceName', 'viaResource', 'viaResourceId', 'viaRelationship']),
     },
 
-    encodedFilters() {
-      return ''
+    emits: ['show-preview'],
+
+    data: () => {
+        return {
+            showActionResponseModal: false,
+            actionResponseData: {},
+        };
     },
 
-    currentTrashed() {
-      return ''
+    computed: {
+        ...mapGetters(['currentUser']),
+
+        currentSearch() {
+            return '';
+        },
+
+        encodedFilters() {
+            return '';
+        },
+
+        currentTrashed() {
+            return '';
+        },
+
+        shouldShowDropdown() {
+            return this.amountDropdownActions > 0 || this.userHasAnyOptions;
+        },
+
+        amountButtonActions() {
+            return this.actions.filter((action) => action.showAsButton == true).length;
+        },
+
+        amountDropdownActions() {
+            return this.actions.filter((action) => !(action.showAsButton == true)).length;
+        },
+
+        shouldShowPreviewLink() {
+            return this.resource.authorizedToView && this.resource.previewHasFields;
+        },
+
+        userHasAnyOptions() {
+            return this.resource.authorizedToReplicate || this.shouldShowPreviewLink || this.canBeImpersonated;
+        },
+
+        canBeImpersonated() {
+            return this.currentUser.canImpersonate && this.resource.authorizedToImpersonate;
+        },
+
+        selectedResources() {
+            return [this.resource.id.value];
+        },
     },
 
-    shouldShowDropdown() {
-      return this.actions.length > 0 || this.userHasAnyOptions
-    },
-
-    shouldShowPreviewLink() {
-      return this.resource.authorizedToView && this.resource.previewHasFields
-    },
-
-    userHasAnyOptions() {
-      return (
-        this.resource.authorizedToReplicate ||
-        this.shouldShowPreviewLink ||
-        this.canBeImpersonated
-      )
-    },
-
-    canBeImpersonated() {
-      return (
-        this.currentUser.canImpersonate && this.resource.authorizedToImpersonate
-      )
-    },
-
-    selectedResources() {
-      return [this.resource.id.value]
-    },
-  },
-}
+    methods: mapActions(['startImpersonating']),
+};
 </script>
